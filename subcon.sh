225a30a26d37d68cbd94ba0119a684c658d182d2
Jiaoben <95892193+williamcanon1@users.noreply.github.com>
2022-10-02 19:15:01 +0800

    Create subcon.sh

---
+++ subcon.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo -i
+
+banben=./banben
+
+wget -qO- -t1 -T2 "https://api.github.com/repos/tindy2013/subconverter/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' > $banben
+
+wget "https://github.com/tindy2013/subconverter/releases/download/$(cat $banben)/subconverter_linux64.tar.gz"
+
+rm -rf $banben
+
+tar zxvf subconverter*
+rm subconverter_linux64.tar.gz
+
+cat > /lib/systemd/system/sub.service << EOF
+[Unit]
+Description=subcon service
+After=network.target syslog.target
+Wants=network.target
+[Service]
+Type=simple
+ExecStart=/root/subconverter/subconverter &
+
+[Install]
+WantedBy=multi-user.target
+
+systemctl start sub
+systemctl enable sub
