1a0d86cb1b1667cd835045a4f3bbadb4e757448b
Jiaoben <95892193+williamcanon1@users.noreply.github.com>
2022-10-03 15:06:06 +0800

    Create ubuntu_docker.sh

---
+++ ubuntu_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+apt update
+apt upgrade -y
+apt install curl vim wget gnupg dpkg apt-transport-https lsb-release ca-certificates -y
+curl -sS https://download.docker.com/linux/debian/gpg | gpg --dearmor > /usr/share/keyrings/docker-ce.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-ce.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -sc) stable" > /etc/apt/sources.list.d/docker.list
+apt update
+apt-get install docker-ce docker-ce-cli containerd.io
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+cat > /etc/docker/daemon.json << EOF
+{
+    "log-driver": "json-file",
+    "log-opts": {
+        "max-size": "20m",
+        "max-file": "3"
+    },
+    "ipv6": true,
+    "fixed-cidr-v6": "fd00:dead:beef:c0::/80",
+    "experimental":true,
+    "ip6tables":true
+}
+EOF
+systemctl restart docker
