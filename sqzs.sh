dffea6eeb5ec90947e213e0a39a94efede79a12d
Jiaoben <95892193+williamcanon1@users.noreply.github.com>
2022-11-19 15:00:08 +0800

    Update sqzs.sh

--- sqzs.sh
+++ sqzs.sh
@@ -15,5 +15,10 @@ read -p "输入域名" website
 acme.sh  --issue -d $website  --standalone -k ec-256
 
 echo 安装证书 
-mkdir /root/ssl
-acme.sh --installcert -d $website --ecc  --key-file   /root/ssl/server.key   --fullchain-file  /root/ssl/server.crt 
+read  -e -p "输入安装路径" Luji
+if [ -e $Luji ];then
+        echo 文件存在
+else
+        mkdir -p $Luji
+fi
+acme.sh --installcert -d $website --ecc  --key-file   $Luji/server.key   --fullchain-file  $Luji/server.crt 
