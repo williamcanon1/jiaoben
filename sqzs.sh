8e6e01a40b391e9ed028b773a173fb8323eefc96
Jiaoben <95892193+williamcanon1@users.noreply.github.com>
2022-11-19 15:12:44 +0800

    Update sqzs.sh

--- sqzs.sh
+++ sqzs.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-echo 申请证书
-
-
+echo 安装acme
 curl https://get.acme.sh | sh #安装acme
+echo 安装socat
 apt install socat  #安装socat
-
+echo 创建软链
 ln -s  /root/.acme.sh/acme.sh /usr/local/bin/acme.sh
+echo 注册
 acme.sh --register-account -m my@example.com
+echo 开放 防火墙
 ufw allow 80
 acme.sh --set-default-ca --server letsencrypt
+
+
+read -p "输入申请方式   1为tcp方式    2为dns方式     " Fangshi
+if [ $Fangshi -eq 1 ];then
+
 read -p "输入域名" website
-
 acme.sh  --issue -d $website  --standalone -k ec-256
-
 echo 安装证书 
 read  -e -p "输入安装路径" Luji
 if [ -e $Luji ];then
@@ -22,3 +26,6 @@ else
         mkdir -p $Luji
 fi
 acme.sh --installcert -d $website --ecc  --key-file   $Luji/server.key   --fullchain-file  $Luji/server.crt 
+
+elif [ $Fangshi -eq 2 ];then
+echo 开始已dns方式获取证书
