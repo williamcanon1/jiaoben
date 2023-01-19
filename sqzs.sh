9e3e42ef31134ca6866107dd3d1b029d926b7eb2
Jiaoben <95892193+williamcanon1@users.noreply.github.com>
2023-01-19 19:06:29 +0800

    Update sqzs.sh

--- sqzs.sh
+++ sqzs.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
+if [ -e /root/.acme.sh/acme.sh ];then
+echo acme已经安装 开始申请证书
 
-echo 安装acme
+read -p "输入申请方式   1为tcp方式    2为dns方式     " Fangshi
+if [ $Fangshi -eq 1 ];then
+
+read -p "输入域名" website
+acme.sh  --issue -d $website  --standalone -k ec-256
+echo 安装证书 
+read  -e -p "输入安装路径" Luji
+if [ -e $Luji ];then
+        echo 文件存在
+else
+        mkdir -p $Luji
+fi
+acme.sh --installcert -d $website --ecc  --key-file   $Luji/server.key   --fullchain-file  $Luji/server.crt 
+
+elif [ $Fangshi -eq 2 ];then
+echo 开始已dns方式获取证书
+read -p "输入key   " Key
+read -p "输入邮箱   " Mail
+export CF_Key="$Key"
+export CF_Email=$Mail
+read -p "输入域名" website
+
+echo 开始申请 证书|
+acme.sh --issue --dns dns_cf -d $website -d *.$website -k ec-256
+
+echo 安装证书 
+read  -e -p "输入安装路径" Luji
+if [ -e $Luji ];then
+        echo 文件存在
+else
+        mkdir -p $Luji
+fi
+acme.sh --installcert -d $website --ecc  --key-file   $Luji/server.key   --fullchain-file  $Luji/server.crt 
+else
+
+echo 请输入1 或 2 你输 的什么几把玩意。
+
+fi
+
+else
+echo acme未安装--开始安装acme
 curl https://get.acme.sh | sh #安装acme
 echo 安装socat
 apt install socat  #安装socat
@@ -50,4 +92,5 @@ else
 
 echo 请输入1 或 2 你输 的什么几把玩意。
 
+fi
 fi
