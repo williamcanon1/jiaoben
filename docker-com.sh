f45bd262bb8db5bc60bcc77e78a66c21a36fcb96
Jiaoben <95892193+williamcanon1@users.noreply.github.com>
2023-05-20 09:19:17 +0800

    Create docker-com.sh

---
+++ docker-com.sh
@@ -0,0 +1,16 @@
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+cat > /etc/docker/daemon.json << EOF
+{
+    "log-driver": "json-file",
+    "log-opts": {
+        "max-size": "20m",
+        "max-file": "3"
+    },
+    "ipv6": true,
+    "fixed-cidr-v6": "fd00:dead:beef:c0::/80",
+    "experimental":true,
+    "ip6tables":true
+}
+EOF
+systemctl restart docker
