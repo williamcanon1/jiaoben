b2e2b1a1a08c3a85ba677285687e9ac3cda3d946
Jiaoben <95892193+williamcanon1@users.noreply.github.com>
2024-12-17 20:58:20 +0800

    Create tanzhen.sh

---
+++ tanzhen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+mkdir tanzhen
+
+read -p "输入key" Kkkey 
+
+cat > docker-compose.yaml < EOF
+services:
+  beszel-agent:
+    image: "henrygd/beszel-agent"
+    container_name: "beszel-agent"
+    restart: unless-stopped
+    network_mode: host
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock:ro
+      # monitor other disks / partitions by mounting a folder in /extra-filesystems
+      # - /mnt/disk/.beszel:/extra-filesystems/sda1:ro
+    environment:
+      PORT: 45876
+      KEY: "Kkkey"
+EOF
+
+sudo apt update &&sudo apt install docker-compose.yaml -y
+
+sudo docker-compose up -d 
